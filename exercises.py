f0c6d57641b5ba25ae05857cd5b12c8702f5e823
Ed Gonzales <edgonzales6@gmail.com>
2023-12-06 09:05:53 -0800

    Exercise 1 complete

---
+++ exercises.py
@@ -0,0 +1,15 @@
+# Exercise 1
+# Create a list named students containing some student names (strings).
+# Print out the second student's name.
+# Print out the last student's name.
+
+students = ['Tim', 'Arnoldo', 'Lalo']
+print(students[1])
+print(students[2])
+
+
+# Exercise 2
+
+# your solution here
+
+# etc.....
