3db851a07b3719e8bc3524d258475170a881926a
Ed Gonzales <edgonzales6@gmail.com>
2023-12-06 09:14:16 -0800

    Exercise 2 complete

--- exercises.py
+++ exercises.py
@@ -9,7 +9,9 @@ print(students[2])
 
 
 # Exercise 2
-
-# your solution here
-
-# etc.....
+# Create a tuple named foods containing the same number of foods (strings) as 
+# there are names in the students list. Use a for loop to print out the string 
+# "[food goes here] is a good food".
+foods = ('Cauliflower', 'Green Beans', 'Orange')
+for food in foods:
+    print (f"{food} is good for you")
